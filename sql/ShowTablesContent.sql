a17b83af7cccc81caf7b71d3c596651a3885ceae
Kia Hadizade <kiahadizade3077@gmail.com>
2026-05-31 10:11:59 +0330

    A Query for Retriving Contents of Tables

---
+++ sql/ShowTablesContent.sql
@@ -0,0 +1,58 @@
+use StreamDB
+
+SELECT *
+FROM Users
+
+SELECT *
+FROM Content
+
+SELECT *
+FROM Genre
+
+SELECT *
+FROM Actor
+
+SELECT *
+FROM Categorized_as
+
+SELECT *
+FROM Performed_by
+
+SELECT *
+FROM Subscription
+
+SELECT *
+FROM Payment
+
+SELECT *
+FROM Rating
+
+SELECT *
+FROM Review
+
+SELECT *
+FROM Favorites
+
+SELECT *
+FROM Episode
+
+SELECT *
+FROM History
+
+SELECT *
+FROM Download
+
+--DELETE FROM Users
+--DELETE FROM Content
+--DELETE FROM Genre
+--DELETE FROM Actor
+--DELETE FROM Categorized_as
+--DELETE FROM Performed_by
+--DELETE FROM Subscription
+--DELETE FROM Payment
+--DELETE FROM Rating
+--DELETE FROM Review
+--DELETE FROM Favorites
+--DELETE FROM Episode
+--DELETE FROM History
+--DELETE FROM Download
