20486bffb6b6f9ffdbcfb789c87a692d8e008256
Kia Hadizade <kiahadizade3077@gmail.com>
2026-05-31 11:57:10 +0330

    feat(api): Add Configs To Connect To SSMS

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const config = {
+    user: "Admin",
+    password: "adminadmin",
+
+    server: "DESKTOP-IFGT9CN",
+    port: 2901,
+    database: "StreamDB",
+
+    options: {
+        trustServerCertificate: true,
+        encrypt: false
+    }
+}
