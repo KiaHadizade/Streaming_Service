fa1117f3a4768ce1d97ee3a6a3dc9a91dd953685
Kia Hadizade <kiahadizade3077@gmail.com>
2026-06-06 22:36:00 +0330

    feat: Update Admin Middleware

--- Stream_node/middleware/isAdmin.js
+++ Stream_node/middleware/isAdmin.js
@@ -1,6 +1,6 @@
 export function isAdmin(req, res, next) {
-
-    if(req.user.role !== "admin") {
+    const role = req.query.role
+    if(role !== "admin") {
         return res.status(403).send("Access denied")
     }
 
