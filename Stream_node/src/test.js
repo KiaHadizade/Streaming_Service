ff1b733866a1ae5be3194d1503d3680dfa5f31e4
Kia Hadizade <kiahadizade3077@gmail.com>
2026-06-06 12:57:34 +0330

    Use environment variable

--- Stream_node/src/test.js
+++ Stream_node/src/test.js
@@ -6,7 +6,6 @@ try {
     await sql.connect(config)
     console.log("Connected to SQL Server!")
 
-    // SELECT DB_NAME() AS databaseName
     const result = await sql.query("SELECT @@VERSION AS version") // OR: SELECT DB_NAME() AS dbName
     console.log(result.recordset)
     await sql.close()
