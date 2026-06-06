a1e147026f5b60558eab24b226793b70d0320ecc
Kia Hadizade <kiahadizade3077@gmail.com>
2026-06-06 12:57:06 +0330

    Use environment variable

--- Stream_node/src/config.js
+++ Stream_node/src/config.js
@@ -1,10 +1,9 @@
 export const config = {
-    user: "Admin",
-    password: "adminadmin",
-
-    server: "DESKTOP-IFGT9CN",
-    port: 2901,
-    database: "StreamDB",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT),
 
     options: {
         trustServerCertificate: true,
--- Stream_node/src/test.js
+++ Stream_node/src/test.js
@@ -1,20 +1,17 @@
 import sql from "mssql"
+import "dotenv/config"
 import { config } from "./config.js"
 
-async function test() {
-    try {
-        await sql.connect(config)
-        console.log("Connected to SQL Server!")
+try {
+    await sql.connect(config)
+    console.log("Connected to SQL Server!")
 
-        // SELECT DB_NAME() AS databaseName
-        const result = await sql.query`
-            SELECT @@VERSION AS version
-        `
-        console.log(result.recordset)
-        await sql.close()
+    // SELECT DB_NAME() AS databaseName
+    const result = await sql.query("SELECT @@VERSION AS version") // OR: SELECT DB_NAME() AS dbName
+    console.log(result.recordset)
+    await sql.close()
 
-    } catch(err) {
-        console.error(err)
-    }
+} catch(err) {
+    console.error("Connection failed:")
+    console.error(err)
 }
-test()
